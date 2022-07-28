b0bc654a8ee8400b91fb486a847cd7ec0d808e26
Alba <104780279+Aemota@users.noreply.github.com>
2022-07-28 13:42:10 +0200

    Add files via upload

---
+++ Get_Next_Line (125%)/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albjimen <albjimen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/07 12:56:07 by albjimen          #+#    #+#             */
+/*   Updated: 2022/07/14 12:27:31 by albjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_strlen(char *str);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*ft_get_line(char *str);
+char	*ft_new_line(char *line);
+char	*ft_read_line(int fd, char *backup);
+
+#endif
